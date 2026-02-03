94c5399c463292575d946bcc29f694687c41020f
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-02-03 17:15:10 +0100

    log de connexion + choix du port dans .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import type {Request, Response, NextFunction} from 'express';
 
 export const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
 export const server = app.listen(port);
+
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] Requête reçue : ${req.method} ${req.url}`);
+  next(); // Passe à la prochaine fonction middleware ou route
+});
+
 
 export function stopServer() {
   server.close();
