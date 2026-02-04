d6705ed46236486884f2300bd567128cb646dfc2
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-02-04 14:04:50 +0100

    ajout de pokemons

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -51,6 +51,61 @@ async function main() {
       imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'
     }
   });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Pikachu',
+      pokedexId: 25,
+      type: { connect: { name: 'Electric'}},
+      lifePoints: 35,
+      size: 0.4,
+      weight: 6.0,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'
+    }
+  });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Squirtle',
+      pokedexId: 7,
+      type: { connect: { name: 'Water'}},
+      lifePoints: 44,
+      size: 0.5,
+      weight: 9.0,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+    }
+  });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Bulbasaur',
+      pokedexId: 1,
+      type: { connect: { name: 'Grass'}},
+      lifePoints: 45,
+      size: 0.7,
+      weight: 6.9,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    }
+  });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Gengar',
+      pokedexId: 94,
+      type: { connect: { name: 'Ghost'}},
+      lifePoints: 60,
+      size: 1.5,
+      weight: 40.5,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png'
+    }
+  });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Dragonite',
+      pokedexId: 149,
+      type: { connect: { name: 'Dragon'}},
+      lifePoints: 91,
+      size: 2.2,
+      weight: 210.0,
+      imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png'
+    }
+  });
 
   console.log('Seed completed!');
 }
