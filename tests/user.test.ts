6797e96867bdbee9fc55064f278da19a212f670f
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-03-17 19:41:07 +0100

    test POST /users/login

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -53,16 +53,65 @@ describe('User API', () => {
     });
   });
 
-  /*describe('POST /login', () => {
+  describe('POST /users/login', () => {
     it('should login a user and return a token', async () => {
-      const user = {};
+      const mockUser = {
+        id: 3,
+        email: '3@mail.com',
+        password: 'encryptedpassword',
+      };
+
+      prismaMock.user.findUnique.mockResolvedValue(mockUser);
+
       const token = 'mockedToken';
+      const response = await request(app)
+          .post('/users/login')
+          .send({
+            email: '3@mail.com',
+            password: 'truePassword',
+          })
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual({
-        token,
-        message: 'Connexion réussie',
+        message: 'Connexion successfull !',
+        token: token,
+        user: {
+          id: 3,
+          email: '3@mail.com',
+        },
       });
     });
-  });*/
+
+    it('should return 401 if user does not exist', async () => {
+      prismaMock.user.findUnique.mockResolvedValue(null);
+
+      const response = await request(app)
+          .post('/users/login')
+          .send({
+            email: 'unknown@mail.com',
+            password: 'truePassword',
+          });
+
+      expect(response.status).toBe(401);
+      expect(response.body).toEqual({ error: 'This user does not exist' });
+    });
+  });
+
+  it('should return 401 if password is incorrect', async () => {
+    prismaMock.user.findUnique.mockResolvedValue({
+      id: 3,
+      email: '3@mail.com',
+      password: 'encryptedpassword',
+    });
+
+    const response = await request(app)
+        .post('/users/login')
+        .send({
+          email: '3@mail.com',
+          password: 'wrongPassword',
+        });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({ error: 'Incorrect password' });
+  });
 });
