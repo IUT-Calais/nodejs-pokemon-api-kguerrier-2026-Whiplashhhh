df6acb97701ddb680994aa66f7c6387c3a47a97d
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-03-17 18:40:30 +0100

    test GET /users

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -3,16 +3,40 @@ import { app } from '../src';
 import { prismaMock } from './jest.setup';
 
 describe('User API', () => {
-  describe('POST /users', () => {
+  describe('GET /users', () => {
+    it('should fetch all Users', async () => {
+      const mockUsers = [
+        {
+          id: 1,
+          email: "1@mail.com",
+          password: "encryptedpassword"
+        },
+        {
+          id: 2,
+          email: "2@mail.com",
+          password: "encryptedpassword"
+        }
+      ];
+
+      prismaMock.user.findMany.mockResolvedValue(mockUsers);
+
+      const response = await request(app).get('/users');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(mockUsers);
+    })
+  })
+
+  /*describe('POST /users', () => {
     it('should create a new user', async () => {
       const createdUser = {};
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(createdUser);
     });
-  });
+  });*/
 
-  describe('POST /login', () => {
+  /*describe('POST /login', () => {
     it('should login a user and return a token', async () => {
       const user = {};
       const token = 'mockedToken';
@@ -23,5 +47,5 @@ describe('User API', () => {
         message: 'Connexion réussie',
       });
     });
-  });
+  });*/
 });
