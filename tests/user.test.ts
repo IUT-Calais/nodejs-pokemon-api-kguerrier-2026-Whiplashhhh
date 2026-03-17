ffac17fefec928525eeb04eb177dd96cf3e911cf
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-03-17 18:51:02 +0100

    test POST /users

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -27,14 +27,31 @@ describe('User API', () => {
     })
   })
 
-  /*describe('POST /users', () => {
+  describe('POST /users', () => {
     it('should create a new user', async () => {
-      const createdUser = {};
+      const createdUser = {
+        id: 3,
+        email: "3@mail.com",
+        password: "encryptedpassword"
+      };
+
+      //vérif du mail
+      prismaMock.user.findUnique.mockResolvedValueOnce(null)
+
+      prismaMock.user.create.mockResolvedValue(createdUser)
+
+      const response = await request(app)
+          .post('/users')
+          .set('Authorization', 'Bearer mockedToken')
+          .send({
+            email: "3@mail.com",
+            password: "encryptedpassword"
+          });
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(createdUser);
     });
-  });*/
+  });
 
   /*describe('POST /login', () => {
     it('should login a user and return a token', async () => {
